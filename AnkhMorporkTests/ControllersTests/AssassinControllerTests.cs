538b8a3af7a55d4ae5af6c16941373f9170dc257
AnneST20 <bulik2004x@gmail.com>
2022-04-11 13:05:45 +0300

    AssassinsControllerTests.cs - fixed tests - added reward

--- AnkhMorporkTests/ControllersTests/AssassinControllerTests.cs
+++ AnkhMorporkTests/ControllersTests/AssassinControllerTests.cs
@@ -8,7 +8,7 @@ namespace AnkhMorporkTests.ControllersTests
     public class AssassinControllerTests
     {
         [TestMethod]
-        public void ApplyGuildRequest_NotEnoughMoney_AliveFalse()
+        public void ApplyGuildRequest_NotEnoughMoneyForContract_AliveFalse()
         {
             var _falseContext = new GuildsContext();
             var _falseInventory = new Inventory { Money = 3m, Beers = 0 };
@@ -18,7 +18,25 @@ namespace AnkhMorporkTests.ControllersTests
             _falseContext.Assassins.Add(new Assassin { Id = 1, MinReward = 5, MaxReward = 12, Missions = 0 });
             _falseContext.SaveChanges();
 
-            var controller = new AssassinsController(_falseContext, _falseInventory);
+            var controller = new AssassinsController(_falseContext, _falseInventory) { Reward = 3m };
+            var result = controller.ApplyGuildRequest();
+
+            Assert.IsFalse(result);
+
+        }
+
+        [TestMethod]
+        public void ApplyGuildRequest_TooMuchMoneyForContract_AliveFalse()
+        {
+            var _falseContext = new GuildsContext();
+            var _falseInventory = new Inventory { Money = 3m, Beers = 0 };
+
+            _falseContext.Assassins.Add(new Assassin { Id = 1, MinReward = 5, MaxReward = 7, Missions = 0 });
+            _falseContext.Assassins.Add(new Assassin { Id = 1, MinReward = 7, MaxReward = 10, Missions = 0 });
+            _falseContext.Assassins.Add(new Assassin { Id = 1, MinReward = 5, MaxReward = 12, Missions = 0 });
+            _falseContext.SaveChanges();
+
+            var controller = new AssassinsController(_falseContext, _falseInventory) { Reward = 15m };
             var result = controller.ApplyGuildRequest();
 
             Assert.IsFalse(result);
@@ -36,7 +54,7 @@ namespace AnkhMorporkTests.ControllersTests
             _falseContext.Assassins.Add(new Assassin { Id = 1, MinReward = 5, MaxReward = 12, Missions = 1 });
             _falseContext.SaveChanges();
 
-            var controller = new AssassinsController(_falseContext, _falseInventory);
+            var controller = new AssassinsController(_falseContext, _falseInventory) { Reward = 3m };
             var result = controller.ApplyGuildRequest();
 
             Assert.IsFalse(result);
